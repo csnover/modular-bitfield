7cfe99328b15bc5f5f5625ed84e3d1dffe193cb5
Colin Snover <github.com@zetafleet.com>
2025-03-13 15:12:20 -0500

    Silence warning about unused braces
    This is caused by braces around `#actual_bits`. Removing
    these requires extra work since they are required in other
    places, so just suppress the warning.

--- impl/src/bitfield/expand.rs
+++ impl/src/bitfield/expand.rs
@@ -377,6 +377,7 @@ impl BitfieldStruct {
                 ReprKind::U128 => quote! { IsU128Compatible },
             };
             quote_spanned!(span=>
+                #[allow(unused_braces)]
                 impl ::core::convert::From<#prim> for #ident
                 where
                     [(); #actual_bits]: ::modular_bitfield::private::#trait_check_ident,
@@ -387,6 +388,7 @@ impl BitfieldStruct {
                     }
                 }
 
+                #[allow(unused_braces)]
                 impl ::core::convert::From<#ident> for #prim
                 where
                     [(); #actual_bits]: ::modular_bitfield::private::#trait_check_ident,
